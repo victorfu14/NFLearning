1cb0601963704213711a3e6474dbaf8cd6f76e5d
aosun3 <aosun3@illinois.edu>
2019-11-21 17:55:01 -0600

    Update network_1.py

--- network_1.py
+++ network_1.py
@@ -1,61 +1,20 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-
-
-
-# In[4]:
-
-
 import tensorflow as tf
-import numpy as np
-x = tf.placeholder(tf.float32, shape=(None, 784))
-y = tf.placeholder('float')
-def multilayer_perceptron(x, weights, biases):
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.relu(layer_1)
-    out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
-    return out_layer
+mnist = tf.keras.datasets.mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+print(x_train.shape,y_train.shape)
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-n_nodes_hl1 = 500
-n_nodes_hl2 = 500
-n_nodes_hl3 = 1
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
 
-def n_n(x):
-    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
-    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
-
-    out_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
-    x1=multilayer_perceptron(x, hidden_1_layer['weights'], hidden_1_layer['biases'])
-    x2=multilayer_perceptron(x1,hidden_2_layer['weights'],hidden_2_layer['biases'])
-    x3=multilayer_perceptron(x2,out_layer['weights'],out_layer['biases'])
-    return x3
-
-def train(x,y):
-    pred=n_n(x)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for epoch in range(20):
-            _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))  
-        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-
-x=np.zeros((10,784))
-y=np.zeros((10,1))
-train(x,y)
-
-
-# In[ ]:
-
-
-
-
+model.fit(x_train, y_train, epochs=2)
+model.evaluate(x_test, y_test)
