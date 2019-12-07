2ab7c7edbccf64539747c3dc7932d0a4f46906a8
aosun3 <aosun3@illinois.edu>
2019-12-06 20:13:42 -0600

    Update network_1.py

--- network_1.py
+++ network_1.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 import pandas as pd
 data1=pd.read_csv("training_data.csv")
-pd1=data1.drop(["Year", "Week", "Home", "Away"], axis=1)
+pd1=data1.drop(["Year", "Week", "Home", "Away","ScoreDiff"], axis=1)
 pd2=pd1.drop(pd1.columns[0], axis = 1) 
 finallabel=pd2.Win.values
 finaltraining=pd2.drop(["Win"], axis=1)
@@ -12,7 +12,7 @@ print(finaltraining.shape)
 
 
 data2=pd.read_csv("testing_data.csv")
-pdt=data2.drop(["Year", "Week", "Home", "Away"], axis=1)
+pdt=data2.drop(["Year", "Week", "Home", "Away","ScoreDiff"], axis=1)
 pdt2=pdt.drop(pdt.columns[0], axis = 1) 
 teslabel=pdt2.Win.values
 testinp=pdt2.drop(["Win"], axis=1).values
@@ -20,17 +20,19 @@ testinp=pdt2.drop(["Win"], axis=1).values
 
 #(x_train, y_train),(x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 #print(x_train.shape,y_train.shape)
-#x_train, x_test = x_train / 255.0, x_test / 255.0
+#testinp, finaltraining = testinp / 10.0, finaltraining / 10.0
 #x1=np.zeros((60000,28,28))
 #y1=np.zeros((60000,1))
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(1200, activation='relu'),
-    tf.keras.layers.Dropout(0.9),
+    #tf.keras.layers.Dropout(0.9),
     tf.keras.layers.Dense(780, activation='relu'),
-    tf.keras.layers.Dropout(0.9),
+    #tf.keras.layers.Dropout(0.9),
     tf.keras.layers.Dense(520, activation='relu'),
-    tf.keras.layers.Dropout(0.9),
+    #tf.keras.layers.Dropout(0.9),
     tf.keras.layers.Dense(259, activation='relu'),
+    tf.keras.layers.Dense(120, activation='relu'),
+    tf.keras.layers.Dense(60, activation='relu'),
     tf.keras.layers.Dense(2, activation='softmax')
 ])
 
@@ -38,5 +40,5 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(finaltraining, finallabel, epochs=10)
+model.fit(finaltraining, finallabel, epochs=7)
 model.evaluate(testinp, teslabel)
