28f96c70abbd4e17fcf12bdc8bfd4cfb929abada
aosun3 <aosun3@illinois.edu>
2019-12-07 19:59:01 -0600

    Update Insetting_model_into_Django.py

--- Insetting_model_into_Django.py
+++ Insetting_model_into_Django.py
@@ -8,5 +8,5 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 imported=tf.saved_model.load("/Users/sunao2000/Desktop") #this is code of how the model is imported
-outputs = imported(tf.convert_to_tensor(testinp, dtype=tf.float32)) #the final output, the variable "testinp" is any numpy array with shape[x,11]
+outputs = imported(tf.convert_to_tensor(testinp, dtype=tf.float32)) #This is the final output, the variable "testinp" is any numpy array with shape[x,11]
 
