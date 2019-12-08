21577ccdfedaaf7b2f61535376c5a1eaa0bc9553
aosun3 <aosun3@illinois.edu>
2019-12-07 21:27:32 -0600

    Update Inseting_model_into_Django.py

--- Inseting_model_into_Django.py
+++ Inseting_model_into_Django.py
@@ -12,7 +12,6 @@ inputdata=xxxxxxx #the numpyarray with size[x,11]
 imported=tf.saved_model.load("/Users/sunao2000/Desktop") #this is code of how the model is imported, please change this directory into correct one.
 outputs = imported(tf.convert_to_tensor(inputdata, dtype=tf.float32))#This is the final output, the variable "inputdata" is any numpy array with shape[x,11]
 output=outputs.numpy()
-#The output with size[x,2] numpyarray type, such as [0.4323,0.547]. 
 final_output=[]
 output=outputs.numpy()
 for i in range(output.shape[0]):
