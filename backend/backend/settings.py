6289ff3862f93107aef6d7313c15568b273986b3
yiweifu3 <yiweifu3@illinois.edu>
2019-12-11 13:15:24 -0600

    deploy website

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFileStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 
