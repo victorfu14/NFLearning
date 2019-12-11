17aeb88673b3955cc4517d580618057bdac74680
yiweifu3 <yiweifu3@illinois.edu>
2019-12-11 14:24:54 -0600

    modify settings and urls

--- backend/settings.py
+++ backend/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'static/bundles')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
